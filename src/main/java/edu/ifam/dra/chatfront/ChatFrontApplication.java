6af0cb9d464c3dbc84a239da28b80691a806f891
unknown <gabrielsouzaneves3@gmail.com>
2023-09-27 17:23:15 -0400

    firt commit

---
+++ src/main/java/edu/ifam/dra/chatfront/ChatFrontApplication.java
@@ -0,0 +1,13 @@
+package edu.ifam.dra.chatfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatFrontApplication.class, args);
+	}
+
+}
